e9e1c17094f52fd4a5f4ffc8b213bc1afe2cb726
schristoff <28318173+schristoff@users.noreply.github.com>
2023-11-03 20:50:29 -0600

    trying to clean it up?
    Signed-off-by: schristoff <28318173+schristoff@users.noreply.github.com>

--- pkg/spin/build.go
+++ pkg/spin/build.go
@@ -2,43 +2,33 @@ package spin
 
 import (
 	"context"
+	"text/template"
 
 	"get.porter.sh/porter/pkg/exec/builder"
+	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 )
 
+const dockerfileLines = `RUN apt-get update && apt-get install -y wget tar && \
+wget https://github.com/fermyon/spin/releases/download/v2.0.0-rc.1/spin-v2.0.0-rc.1-linux-amd64.tar.gz --progress=dot:giga && \
+tar -xvf spin-v2.0.0-rc.1-linux-amd64.tar.gz -C /usr/bin/ && \
+spin plugin install -y --url https://github.com/fermyon/platform-plugin/releases/download/canary/platform.json`
+
 // BuildInput represents stdin passed to the mixin for the build command.
 type BuildInput struct {
-	Config MixinConfig
+	Config *MixinConfig
 }
-
-// MixinConfig represents configuration that can be set on the spin mixin in porter.yaml
-// mixins:
-// - spin:
-//	  clientVersion: "v0.0.0"
 
 type MixinConfig struct {
 	ClientVersion string `yaml:"clientVersion,omitempty"`
+	FermyonCloud  bool   `yaml:"fermyonCloud"`
 }
 
-// This is an example. Replace the following with whatever steps are needed to
-// install required components into
-// const dockerfileLines = `RUN apt-get update && \
-// apt-get install gnupg apt-transport-https lsb-release software-properties-common -y && \
-// echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ stretch main" | \
-//    tee /etc/apt/sources.list.d/azure-cli.list && \
-// apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
-// 	--keyserver packages.microsoft.com \
-// 	--recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF && \
-// apt-get update && apt-get install azure-cli
-// `
-
-// Build will generate the necessary Dockerfile lines
-// for an invocation image using this mixin
 func (m *Mixin) Build(ctx context.Context) error {
 
-	// Create new Builder.
-	var input BuildInput
+	input := BuildInput{
+		Config: &m.config, // Apply config directly to the mixin
+	}
 
 	err := builder.LoadAction(ctx, m.RuntimeConfig, "", func(contents []byte) (interface{}, error) {
 		err := yaml.Unmarshal(contents, &input)
@@ -48,16 +38,10 @@ func (m *Mixin) Build(ctx context.Context) error {
 		return err
 	}
 
-	suppliedClientVersion := input.Config.ClientVersion
-
-	if suppliedClientVersion != "" {
-		m.ClientVersion = suppliedClientVersion
+	tmpl, err := template.New("spin").Parse(dockerfileLines)
+	if err != nil {
+		return errors.Wrapf(err, "error parsing terraform mixin Dockerfile template")
 	}
 
-	//fmt.Fprintf(m.Out, dockerfileLines)
-
-	// Example of pulling and defining a client version for your mixin
-	// fmt.Fprintf(m.Out, "\nRUN curl https://get.helm.sh/helm-%s-linux-amd64.tar.gz --output helm3.tar.gz", m.ClientVersion)
-
-	return nil
+	return tmpl.Execute(m.Out, tmpl)
 }
---
+++ pkg/spin/spin.go
@@ -0,0 +1,44 @@
+package spin
+
+import (
+	"context"
+	"os"
+
+	"get.porter.sh/porter/pkg/runtime"
+)
+
+const defaultClientVersion string = "v2.0.0"
+const defaultFermyonCloud bool = false
+
+type Mixin struct {
+	runtime.RuntimeConfig
+	ClientVersion string
+	config        MixinConfig
+}
+
+// commandPreRun runs setup tasks applicable for every action
+func (m *Mixin) commandPreRun(ctx context.Context, step *Step) error {
+	if step.FermyonCloud {
+		return nil
+	}
+
+	os.Setenv("BINDLE_PASSWORD", step.BindlePassword)
+	os.Setenv("BINDLE_URL", step.BindleServer)
+	os.Setenv("BINDLE_USERNAME", step.BindleUsername)
+	os.Setenv("HIPPO_PASSWORD", step.HippoPassword)
+	os.Setenv("HIPPO_USERNAME", step.HippoUsername)
+	// os.Setenv("SPIN_AUTH_TOKEN", step.AuthToken)
+
+	return nil
+}
+
+// New azure mixin client, initialized with useful defaults.
+func New() *Mixin {
+	return &Mixin{
+		RuntimeConfig: runtime.NewConfig(),
+		ClientVersion: defaultClientVersion,
+		config: MixinConfig{
+			FermyonCloud: defaultFermyonCloud,
+		},
+	}
+}
